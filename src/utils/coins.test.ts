4425df76ad7ceae6764c6d77e427c0fe1e5bb6e5
Sayam Gautam <sayamgautam1@gmail.com>
2023-10-06 11:37:55 +1030

    add: extra edge cases for coin display and amount input functions

--- src/utils/coins.test.ts
+++ src/utils/coins.test.ts
@@ -12,12 +12,44 @@ describe("coins", () => {
       })
     );
   });
-  it("runs", () => {
+  it("Amount with Only Smallest Coin - 5 Cents", () => {
     const denominations = getCoins(5);
 
     expect(denominations).toEqual(
       expect.objectContaining({
         "5c": 1,
+      })
+    );
+  });
+  it("Large Amount - 789 Dollars and 12 Cents", () => {
+    const denominations = getCoins(78912);
+
+    expect(denominations).toEqual(
+      expect.objectContaining({
+        "1c": 0,
+        "2c": 1,
+        "5c": 0,
+        "10c": 1,
+        "20c": 0,
+        "50c": 0,
+        "1d": 1,
+        "2d": 394,
+      })
+    );
+  });
+  it("amount with various coins", () => {
+    const denominations = getCoins(153);
+
+    expect(denominations).toEqual(
+      expect.objectContaining({
+        "1c": 1,
+        "2c": 1,
+        "5c": 0,
+        "10c": 0,
+        "20c": 0,
+        "50c": 1,
+        "1d": 1,
+        "2d": 0,
       })
     );
   });
